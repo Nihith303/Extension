68bac526eed6110059a50096815e93bbc496895d
Jonnalagadda Nihit <133048404+Nihith303@users.noreply.github.com>
2024-12-11 18:43:46 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,3 @@
-// import React, { useState } from "react";
-// import TabNavigation from "./components/navigator/TabNavigation";
-// import Summary from "./tabs/summary/Summary";
-// import Links from "./tabs/links/Links";
-// import Images from "./tabs/imagestab/Images";
-// import Header from "./tabs/header/Header"; 
-// import Schema from "./tabs/schema/Schema";
-// import PageSpeedTest from "./tabs/pagespeed/PageSpeed";
-// import Footer from "./components/footer/Footer";
-
-// const App = () => {
-//   const [activeTab, setActiveTab] = useState("summary"); 
-
-//   return (
-//     <div>
-//       <TabNavigation activeTab={activeTab} setActiveTab={setActiveTab} />
-
-//       {activeTab === "summary" && <Summary />}
-//       {activeTab === "links" && <Links />}
-//       {activeTab === "images" && <Images />}
-//       {activeTab === "headers" && <Header />} 
-//       {activeTab === "schema" && <Schema />}
-//       {activeTab === "pagespeedtest" && <PageSpeedTest />}
-
-//       <div><Footer /></div>
-//     </div>
-//   );
-// };
-
-// export default App;
-
-
-
-
-
-
-
-
-
-
 import React, { useState } from "react";
 import TabNavigation from "./components/navigator/TabNavigation";
 import Summary from "./tabs/summary/Summary";
@@ -47,7 +7,7 @@ import Header from "./tabs/header/Header";
 import Schema from "./tabs/schema/Schema";
 import PageSpeedTest from "./tabs/pagespeed/PageSpeed";
 import Footer from "./components/footer/Footer";
-import "./App.css"; // Add this for layout styling
+import "./App.css"; 
 
 const App = () => {
   const [activeTab, setActiveTab] = useState("summary");
